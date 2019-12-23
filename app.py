5611f24c2cb644ae7cd3b240c49cb3f67b4a0392
jcorm714 <jcorm714@gmail.com>
2019-12-23 18:17:28 -0500

    Fixed WSGI issue

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, url_for, redirect, render_template, request
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def index():
     return  render_template("index.html")
 
 if(__name__ == '__main__'):
     
-    app.run(debug= True)
+    application.run(debug= True, host='0.0.0.0')
  
